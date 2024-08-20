e0f47868fbac79d1b3a4dc37099019b0e210c769
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-08-20 16:03:25 -0500

    Fix: Updated components

--- src/components/movies/miniComponent/MovieInfo.vue
+++ src/components/movies/miniComponent/MovieInfo.vue
@@ -1,47 +1,45 @@
 <script lang="ts" setup>
-
-import { defineProps } from 'vue'; 
-import type { InfoMovie } from '@/model/movie.info.model';
+import { defineProps } from "vue";
+import type { InfoMovie } from "@/model/movie.info.model";
 
 // Para pasar los props al padre
 const props = defineProps<{
-    movie?: InfoMovie;
+  movie?: InfoMovie;
 }>();
-
 </script>
 
 <template>
-  <h2>{{ props.movie?.Title }}.</h2>
-  <p><strong>Category: </strong> {{ props.movie?.Type }}</p>
-  <p><strong>Year: </strong> {{ props.movie?.Year }}</p>
-  <p><strong>Runtime: </strong> {{ props.movie?.Runtime }}</p>
-  <p><strong>Genre: </strong> {{ props.movie?.Genre }}</p>
-  <p v-if="props.movie?.TotalSeasons">
-    <strong>Seasons: </strong> {{ props.movie?.TotalSeasons }}
-  </p>
-  <p><strong>Rated: </strong> {{ props.movie?.Rated }}</p>
-  <p><strong>Trama: </strong> {{ props.movie?.Plot }}</p>
-  <p><strong>Lenguaje: </strong> {{ props.movie?.Language }}</p>
+  <div>
+    <h2>{{ props.movie?.Title }}.</h2>
+    <p><strong>Category: </strong> {{ props.movie?.Type }}</p>
+    <p><strong>Year: </strong> {{ props.movie?.Year }}</p>
+    <p><strong>Runtime: </strong> {{ props.movie?.Runtime }}</p>
+    <p><strong>Genre: </strong> {{ props.movie?.Genre }}</p>
+    <p v-if="props.movie?.TotalSeasons">
+      <strong>Seasons: </strong> {{ props.movie?.TotalSeasons }}
+    </p>
+    <p><strong>Rated: </strong> {{ props.movie?.Rated }}</p>
+    <p><strong>Trama: </strong> {{ props.movie?.Plot }}</p>
+    <p><strong>Lenguaje: </strong> {{ props.movie?.Language }}</p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
 .movie-info {
-    h2 {
-        color: $primary;
-        font-size: 40px;
-        margin-bottom: 10px;
+  h2 {
+    color: $primary;
+    font-size: 40px;
+    margin-bottom: 10px;
+  }
 
-    }
+  strong {
+    font-size: 20px;
+    color: $primary;
+  }
 
-    strong {
-        font-size: 20px;
-        color: $primary;
-    }
-
-    p {
-        color: $white;
-        font-size: 18px;
-    }
+  p {
+    color: $white;
+    font-size: 18px;
+  }
 }
 </style>
