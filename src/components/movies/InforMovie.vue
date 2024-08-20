68d4281d8ae7268ea615902377e88f54390fd6da
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-08-20 16:01:17 -0500

    Fix: Updated components

--- src/components/movies/InforMovie.vue
+++ src/components/movies/InforMovie.vue
@@ -4,6 +4,8 @@
 import { useRoute } from 'vue-router';
 import { onMounted, computed } from 'vue';
 import { useMoviesStore } from '@/store/movies.store';
+import MovieInfo from './miniComponent/MovieInfo.vue';
+import MovieInfoBottom from './miniComponent/MovieInfoBottom.vue';
 
 // Para la ruta obtener la ruta
 const routa = useRoute();
@@ -14,8 +16,12 @@ const storeMovies = useMoviesStore();
 // Para la pelicula seleccioanda
 const selectMovie = computed(() => storeMovies.movieInformation);
 
-onMounted(() => {
-    storeMovies.getMoviesId(routa.params.id as string);
+onMounted(async () => {
+    try {
+        storeMovies.getMoviesId(routa.params.id as string); 
+    } catch (error) {
+        console.error("Error movie:", error)
+    }
 });
 
 </script>
@@ -29,33 +35,12 @@ onMounted(() => {
             </div>
     
             <div class="movie-info">
-                <h2>{{ selectMovie?.Title }}.</h2>
-                <p><strong>Category: </strong> {{ selectMovie?.Type }}</p>
-                <p><strong>Year: </strong> {{ selectMovie?.Year }}</p>
-                <p><strong>Runtime: </strong> {{ selectMovie?.Runtime }}</p>
-                <p><strong>Genre: </strong> {{ selectMovie?.Genre }}</p>
-                <p v-if="selectMovie?.TotalSeasons"><strong>Seasons: </strong> {{ selectMovie?.TotalSeasons }}</p>
-                <p><strong>Rated: </strong> {{ selectMovie?.Rated }}</p>
-                <p><strong>Trama: </strong> {{ selectMovie?.Plot }}</p>
-                <p><strong>Lenguaje: </strong> {{ selectMovie?.Language }}</p>
+                <MovieInfo v-if="selectMovie" :movie="selectMovie" />
             </div>
         </div>
 
         <div class="movie-info-bottom">
-            <p><strong>Director: </strong> {{ selectMovie?.Director }}</p>
-            <p><strong>Writer: </strong> {{ selectMovie?.Writer }}</p>
-            <p><strong>Released: </strong> {{ selectMovie?.Released }}</p>
-            <p><strong>Country: </strong> {{ selectMovie?.Country }}</p>
-            <p><strong>Awards: </strong> {{ selectMovie?.Awards }}</p>
-            <p><strong>Supporting Actors: </strong> {{ selectMovie?.Actors }}</p>
-            <p><strong>Awards: </strong> {{ selectMovie?.Awards }}</p>
-            <p><strong>DVD: </strong> {{ selectMovie?.DVD }}</p>
-            <p><strong>BoxOffice: </strong> {{ selectMovie?.BoxOffice }}</p>
-            <p><strong>Production: </strong> {{ selectMovie?.Production }}</p>
-            <p><strong>Website: </strong> {{ selectMovie?.Website }}</p>
-            <p><strong>Response: </strong> {{ selectMovie?.Response }}</p>
-            <p v-if="selectMovie?.Ratings && selectMovie?.Ratings.length > 0"><strong>Source: </strong> {{ selectMovie?.Ratings[0].Source }}</p>
-            <p v-if="selectMovie?.Ratings && selectMovie?.Ratings.length > 1"><strong>Value: </strong> {{ selectMovie?.Ratings[1].Value }}</p>
+            <MovieInfoBottom v-if="selectMovie" :movie="selectMovie" />
         </div>
     </div>
 </template>
@@ -89,25 +74,9 @@ onMounted(() => {
 }
 
 .movie-info {
+
     padding: 10px;
     flex: 1;
-
-    h2 {
-        color: $primary;
-        font-size: 40px;
-        margin-bottom: 10px;
-
-    }
-
-    strong {
-        font-size: 20px;
-        color: $primary;
-    }
-
-    p {
-        color: $white;
-        font-size: 18px;
-    }
 }
 
 .movie-info-bottom {
@@ -116,16 +85,5 @@ onMounted(() => {
     display: grid;
     margin-top: 20px;
     grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-
-    p {
-
-        margin: 0;
-        color: $white;
-        padding: 5px;
-        border-radius: 5px;
-        text-overflow: ellipsis;
-        border: 2px solid $primary;
-        background-color: $secondary;
-    }
 }
 </style>
---
+++ src/components/movies/miniComponent/MovieInfoBottom.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+// Imports
+import { defineProps } from 'vue'; 
+import type { InfoMovie } from '@/model/movie.info.model';
+
+// Para pasar los props al padre
+const props = defineProps<{
+    movie: InfoMovie;
+}>();
+</script>
+
+<template>
+  <p><strong>Director: </strong> {{ props.movie?.Director }}</p>
+  <p><strong>Writer: </strong> {{ props.movie?.Writer }}</p>
+  <p><strong>Released: </strong> {{ props.movie?.Released }}</p>
+  <p><strong>Country: </strong> {{ props.movie?.Country }}</p>
+  <p><strong>Awards: </strong> {{ props.movie?.Awards }}</p>
+  <p><strong>Supporting Actors: </strong> {{ props.movie?.Actors }}</p>
+  <p><strong>Awards: </strong> {{ props.movie?.Awards }}</p>
+  <p><strong>DVD: </strong> {{ props.movie?.DVD }}</p>
+  <p><strong>BoxOffice: </strong> {{ props.movie?.BoxOffice }}</p>
+  <p><strong>Production: </strong> {{ props.movie?.Production }}</p>
+  <p><strong>Website: </strong> {{ props.movie?.Website }}</p>
+  <p><strong>Response: </strong> {{ props.movie?.Response }}</p>
+  <p v-if="props.movie.Ratings && props.movie?.Ratings.length > 0">
+    <strong>Source: </strong> {{ props.movie?.Ratings[0].Source }}
+  </p>
+  <p v-if="props.movie.Ratings && props.movie?.Ratings.length > 1">
+    <strong>Value: </strong> {{ props.movie?.Ratings[1].Value }}
+  </p>
+</template>
+
+<style lang="scss" scoped>
+
+.movie-info-bottom {
+  p {
+    margin: 0;
+    color: $white;
+    padding: 5px;
+    border-radius: 5px;
+    text-overflow: ellipsis;
+    border: 2px solid $primary;
+    background-color: $secondary;
+  }
+}
+</style>
